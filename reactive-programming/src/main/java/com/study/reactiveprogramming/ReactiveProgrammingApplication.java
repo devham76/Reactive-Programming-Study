45cf14ddfd755b7be885f60cc9860d480d6d6ea2
devham76 <devham76@gmail.com>
2021-04-15 15:04:23 +0900

    Add files about webclient

--- reactive-programming/src/main/java/com/study/reactiveprogramming/ReactiveProgrammingApplication.java
+++ reactive-programming/src/main/java/com/study/reactiveprogramming/ReactiveProgrammingApplication.java
@@ -1,13 +1,75 @@
+/**
+ * 토비의 봄 TV 12회 스프링 리액티브 프로그래밍 (8) WebFlux
+ * https://www.youtube.com/watch?v=ScH7NZU_zvk
+ * */
 package com.study.reactiveprogramming;
 
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@EnableAsync    // 비동기(다른 스레드)
+@RestController
 @SpringBootApplication
 public class ReactiveProgrammingApplication {
+    static final String URL1 = "http://localhost:8090/service1?req={req}";
+    static final String URL2 = "http://localhost:8090/service2?req={req}";
+
+    @Autowired
+    MyService myService;
+
+    WebClient client = WebClient.create();
+
+    @GetMapping("/rest")
+    public Mono<String> rest(@RequestParam int req) {
+        //spring이 알아서 subscribe호출해준다.
+        // reactive code style 장점 ; 중간중간 끼어들기 좋다.
+        return client.get().uri(URL1, req).exchange()   // return ; Mono<ClientResponse>
+                     .doOnSuccess(c -> log.info("[test] " + c.toString()))
+                     .flatMap(c -> c.bodyToMono(String.class))  // Mono<String>
+                     .flatMap(res1 -> client.get().uri(URL2, res1).exchange())   // Mono<ClientResponse>
+                     .flatMap(c -> c.bodyToMono(String.class))  // Mono<String>
+                     .doOnSuccess(c -> log.info("[test] " + c.toString()))
+                     .flatMap(res2 -> Mono.fromCompletionStage(
+                             myService.work(res2)))   // CompletableFuture<String> -> Mono<Stirng>
+                     .doOnSuccess(c -> log.info("[test] " + c.toString()));
+
+    }
+
+    @GetMapping("/service1")
+    public Mono<String> service1(@RequestParam String req) {
+        return Mono.just(" service1 : " + req);
+    }
+
+    @GetMapping("/service2")
+    public Mono<String> service2(@RequestParam String req) {
+        return Mono.just(" service2 : " + req);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveProgrammingApplication.class, args);
     }
 
+    @Service
+    public static class MyService {
+
+        // 비동기 리턴 가능. String -> Future , CompletableFuture...등으로 returen해야함.
+        @Async
+        public CompletableFuture<String> work(String req) {
+            return CompletableFuture.completedFuture(req + "/asyncwork");
+        }
+    }
 }
--- reactive-programming/src/main/java/lecture/day4/README.md
+++ reactive-programming/src/main/java/lecture/day4/README.md
@@ -1,8 +1,17 @@
-## 토비의 봄 TV 8회 스프링 리액티브 프로그래밍 (4) 자바와 스프링의 비동기 기술
+# 토비의 봄 TV 8회 스프링 리액티브 프로그래밍 (4) 자바와 스프링의 비동기 기술
 - https://www.youtube.com/watch?v=aSTuQiPB4Ns
 
+## 자바에서의 비동기
+- JVM에서 비동기 코드를 어떻게 개발할 수 있을까?
+- 자바는 비동기 프로그래밍을 위한 두가지 모델을 제공한다.
+
+|종류|설명|
+|---|---|
+|Callbacks|비동기 메서드는 반환 값을 가질 수 없지만, 외부 callback매개 변수 (람다나 익명 클래스)를 통해 결과를 호출받을 수 있다.<br> (ex : Swing의 EventListener)  | 
+|Futures|비동기 메서드는 Future를 직시 리턴한다. 비동기 프로세스는 T value를 계산하지만 Future개체는 이에 대한 access를 래핑한다. 값을 즉시 사용할 수 없으며 사용할 수 있을 때까지 값을 polling할 수 있다.<br> (ex: Callable 작업을 실행하는 ExecutorService는 Future 개체를 사용한다.) | 
 
 ### public interface Future<V>
 비동기 적인 무엇인가 작업을 실행하고, 그것에 대한 결과를 가지고있는것
 기존의 스레드말고, 새로운 스레드에서 별개의 작업을 하고, 그 결과를 다른 방법으로 가져와야하는데 가장 기본적인방법이 
 future interface이다.
+
